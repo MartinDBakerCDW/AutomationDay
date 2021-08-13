778ede821bbc0f7c07ff576bc45ead4a80ef6943
MartinDBakerCDW <56068833+MartinDBakerCDW@users.noreply.github.com>
2021-08-13 09:45:31 +0100

    Update azuredeploy.tf

--- terraform/azuredeploy.tf
+++ terraform/azuredeploy.tf
@@ -1,8 +1,8 @@
 #backend
 terraform {
   backend "azurerm" {
-    resource_group_name  = "your-resource-group"
-    storage_account_name = "yourstorageaccount123"
+    resource_group_name  = "rg-uks-terraform"
+    storage_account_name = "samdbterraform"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
